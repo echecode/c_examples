1449d22cc7530909926fe45e7dda0b1ae51bcd3a
HernanEche <hernan.eche@gmail.com>
2014-08-12 11:45:14 -0300

    commit inicial

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/* 
+
+    Este es un comentario en lenguaje C 
+
+*/
+
+// Este es un comentario para compiladores nuevos
+
+int main (void){    
+	  
+      printf("Hola Mundo");  
+       
+      return 0;   
+}
