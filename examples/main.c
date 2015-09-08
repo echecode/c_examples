d3a1afc1a0ab00f804911682fd9f324469a840e8
Hernán Eche <hernan_eche@yahoo.com.ar>
2015-09-08 08:55:07 -0300

    Update main.c

--- examples/main.c
+++ examples/main.c
@@ -1,16 +1,9 @@
 #include<stdio.h>
-
 /* 
-
-    Este es un comentario en lenguaje C.
-
+    Este es un comentario de lenguaje C.
 */
-
-// Este es un comentario para compiladores desde estándar C99  (año 1999) 
-
+// Este es otro comentario para compiladores C99  
 int main (void){    
-	  
       printf("Hola Mundo");  
-       
       return 0;   
 }
