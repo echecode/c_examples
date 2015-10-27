02d36b41dfcf814a52f8385289d3945b8180b7eb
Hernán Eche <hernan_eche@yahoo.com.ar>
2015-10-27 15:44:00 -0300

    Update funcionSuma.c

--- examples/funcionSuma.c
+++ examples/funcionSuma.c
@@ -5,14 +5,10 @@ int suma (int a, int b){
 }
 
 int main(){
-
 	int val1, val2, res;
 	val1=5;
 	val2=3;
-	
 	res=suma(val1, val2);
-	
 	printf("La suma de %d con %d resulta %d",val1, val2, res);
-	
 }
 
