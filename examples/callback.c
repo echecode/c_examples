97c5d683969418e40b4c2d02aea8e058220c057d
Hernán Eche <hernan_eche@yahoo.com.ar>
2016-08-10 09:45:10 -0300

    Update callback.c

--- examples/callback.c
+++ examples/callback.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
-int operar( int (*operacion)(int, int), int a, int b ){
-	return (*operacion)(a,b);	
+int aplicar( int (*operacion)(int, int), int a, int b ){
+	return operacion(a,b);// equivalente a (*operacion)(a,b);	
 }
 
 int suma(int a, int b){
@@ -15,8 +15,8 @@ int producto(int a, int b){
 int main(void) {	
 	int n=3, m=4;
 	
-	printf("\nResultado suma : %d", operar(suma, n, m));
-	printf("\nResultado producto: %d", operar(producto, n, m));
+	printf("\nResultado suma : %d", aplicar(suma, n, m));
+	printf("\nResultado producto: %d", aplicar(producto, n, m));
 	
 	return 0;
 }
