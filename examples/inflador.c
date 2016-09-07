7db30e64de8dfa76a20f163c400b52293314db1e
Hernán Eche <hernan_eche@yahoo.com.ar>
2016-09-07 15:47:03 -0300

    Update inflador.c

--- examples/inflador.c
+++ examples/inflador.c
@@ -1,58 +1,76 @@
 #include<stdio.h>
 #include<windows.h>
 
-int inflador (int sp, int pr, int tiempo){
+
+
+int procesoInflador (int sp, int presionActual, int tiempo){
     int res;
-    if ((tiempo<=0) || (sp==pr)){
+    if ( (tiempo<=0) || (sp==presionActual)){ //si el tiempo termina, o set point = presion actual
           res=0;          
-    }else if(sp<pr){
+    }else if(sp<presionActual){
           res=-1;
-    }else if(sp>pr){
+    }else if(sp>presionActual){
           res=1;
     }
-    
     return res;
 }
           
+
            
 int main (){
+	
     int setPoint, presion, tiempo;
-    do{
-        printf("Ingrese set point:\n");
-        scanf ("\n %d", &setPoint);
-    }while(setPoint<0);
-    do{
-        printf("\nIngrese presion actual:\n");
-        scanf ("\n %d", &presion);        
-    }while(presion<0);
+    
     do{    
-        printf("\nIngrese tiempo:\n");
-        scanf ("\n %d", &tiempo);
-    }while(tiempo<0);
+        printf("\nTiempo total [segundos]\n");
+        scanf ("%d", &tiempo);
+    }while(tiempo<=0);//si el valor no es permitido, repite ingreso
 
     do{
-		Sleep(1000);
-		tiempo=tiempo-1;
-		int opcion=inflador(setPoint, presion, tiempo);
+        printf("\nSet point\n");
+        scanf ("%d", &setPoint);
+    }while(setPoint<0);//si el valor no es permitido, repite ingreso
+    
+    do{
+        printf("\nPresion actual\n",162);
+        scanf ("%d", &presion);        
+    }while(presion<0);//si el valor no es permitido, repite ingreso
+    
+    printf("\n\n");
+    
+    do{
+		Sleep(1000);	//espera 1000ms = 1 s
+		
+		tiempo--;	//resta 1 a la variable tiempo
+		
+		int accion;
+		
+		accion = procesoInflador(setPoint, presion, tiempo);
 
-
-        switch(opcion){
-                   case 0:
-                        printf("DETIENE  p:%d t:%d           \r",presion, tiempo);
+		//Muestra proceso
+        	switch(accion){
+        		   
+                   case 0:                   		
+                        printf("=DETIENE  p:%d t:%d              \r",presion, tiempo);
                         goto salir;
                    break;
                    case -1:
-                        printf(" DESINFLAR  p:%d t:%d         \r",presion, tiempo);
-                        presion=presion-1;
+                        printf("-DESINFLA  p:%d t:%d            \r",presion, tiempo);
+                        presion--;
                    break;
                    case 1:
-                        printf(" INFLAR  p:%d t:%d             \r",presion, tiempo);
-                        presion=presion+1;
+                        printf("+INFLA  p:%d t:%d               \r",presion, tiempo);
+                        presion++;
                    break;                   
-    	}
+                   default:
+                   		printf("Error");
+                   		goto salir;
+                   break;
+    		}
+    	
     }while(1);
     
     salir:
 
-    getch ();
+    getchar();
 }
